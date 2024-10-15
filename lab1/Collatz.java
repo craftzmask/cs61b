d4e7892d8c0ae2a3fb7fc611b9a4547ca9841560
Khanh Chung <khanh.d.chung@gmail.com>
2024-10-15 13:39:26 -0700

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,14 +3,14 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /**
+     * @param int given number
+     * Returns the next Collatz number */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
+        if (n % 2 == 1) {
             return 3 * n + 1;
         } else {
-            return n * 2;
+            return n / 2;
         }
     }
 
